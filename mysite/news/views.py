54c63909d29489414556552207652b3778c9956f
Syringe89 <Syringe89@gmail.com>
2021-01-26 23:10:25 +0700

    Добавлена страничная навигация по новостям и система регистрации/авторизации

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
@@ -8,6 +10,23 @@ from .models import News, Category
 from .utils import MyMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserCreationForm()
+    return render(request, 'news/register.html', {'form': form})
+
+def login(request):
+    return render(request, 'news/login.html')
 
 
 def test(request):
@@ -22,7 +41,9 @@ class HomeNews(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    mixin_prop = 'hello world'
+    # mixin_prop = 'hello world'
+    paginate_by = 1
+
 
     # extra_context = {'title': 'Главная'}
 
@@ -41,6 +62,7 @@ class NewsByCategory(MyMixin, ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 1
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(NewsByCategory, self).get_context_data(**kwargs)
